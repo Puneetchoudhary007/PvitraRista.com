bf6b8f4ea69b2e94edc0bd8230d82b8eaf9db7ac
Puneetchoudhary007 <75292528+Puneetchoudhary007@users.noreply.github.com>
2021-09-23 08:16:36 +0530

    puniCode

--- src/app/model.ts
+++ src/app/model.ts
@@ -3,7 +3,7 @@ import { SignupComponent } from "./sign-up/sign-up.component";
 export class Model {
     constructor( private signupObj: SignupComponent,)
     {
-       let firstname =this.signupObj.FirstnameformControl.value;
-        
+        this.signupObj.firstnameFormControl.value;
+            
     }
 }
